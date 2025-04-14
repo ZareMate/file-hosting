799c7d7cecb7059936d92f7157f584ad3e7663aa
ZareMate <0.zaremate@gmail.com>
2025-04-14 09:43:40 +0200

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   output: "standalone",
   },
+  "allowedDevOrigins": [
+    "http://localhost:3000",
+    "http://10.0.0.1:3000",
+    "http://files.suchodupin.com:3000",
+    "https://files.suchodupin.com:3000",
+  ],
   images: {
     remotePatterns: [
       {
