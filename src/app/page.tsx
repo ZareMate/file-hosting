6522a256b9700628d735a5676fe949465c666359
ZareMate <0.zaremate@gmail.com>
2025-04-14 09:34:06 +0200

    modified:   src/app/page.tsx new file:   src/components/Auth.tsx modified:   src/trpc/server.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { HydrateClient } from "~/trpc/server";
 import FileGrid from "~/app/_components/FileGrid";
 import UploadForm from "~/app/_components/UploadForm";
 import { Toaster } from "react-hot-toast";
+import { signIn, signOut } from "next-auth/react";
 
 export default async function Home() {
   const session = await auth();
@@ -42,14 +43,12 @@ export default async function Home() {
           )}
           {!session?.user && (
           <div className="flex flex-col items-center gap-2">
-            <div className="flex flex-col items-center justify-center gap-4">
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
+            <button
+              onClick={() => (session ? signOut() : signIn())}
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              {session ? "Sign out" : "Sign in"}
+            </button>
           </div>
           )}
         </div>
